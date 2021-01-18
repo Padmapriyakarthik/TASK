64e9f7be09a6df89952f09720f8ba730f931cbce
Padmapriyakarthik <71600824+Padmapriyakarthik@users.noreply.github.com>
2021-01-18 10:50:29 +0530

    Add files via upload

---
+++ Task_4/script.js
@@ -0,0 +1,42 @@
+// create a request variable.
+var request= new XMLHttpRequest();
+//create a connection
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+//send the connection
+request.send();
+//register a event listener.once the data is ready loads the data
+request.onload=function()
+{
+var countrydata=JSON.parse(this.response);
+
+//Question 1.Get all countries from asia continent /"region" using filter function.
+let Asian_region=countrydata.filter((element)=> element.region==="Asia");
+console.log(Asian_region);
+
+
+// Question 2. Get all the countries with population less than 2 lakhs using filter function.
+let population_less=countrydata.filter((element)=> element.population<200000);
+console.log(population_less);
+
+//Question 3. Print name,capital,flag using foreach function.
+
+countrydata.forEach(element => { 
+    console.log("Country Name: "+ element.name, "Capital: " +element.capital,"Flag:" +element.flag);});
+
+// Question 4. Print the total population of countries using reduce function.
+let totalpopulation=countrydata.reduce((accumulator,element)=> accumulator+element.population,0);
+console.log(totalpopulation);
+
+//Question 5.Print the country which uses us dollar as currency
+var dollar=[];
+countrydata.forEach(element=>{
+    element.currencies.forEach((currency)=>
+    {
+        if(currency.code==="USD")
+        {
+            console.log(element.name,currency.code);
+        }
+    })
+})
+
+}
